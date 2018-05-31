9574e6908d1d588ec106302ecaa45a34f84464d5
walid bezzaoui <walid.bezzaoui.1@ens.etsmtl.ca>
2018-05-31 12:26:00 -0400

    H2 database Service

---
+++ database-service/src/main/java/com/pfe/ldb/database/configuration/DatabaseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.pfe.ldb.database.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseServiceApplication.class, args);
+	}
+}
