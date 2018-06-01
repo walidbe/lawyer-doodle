6d229bffbca068fc2067c08237c4a0baa822794a
walid bezzaoui <walid.bezzaoui.1@ens.etsmtl.ca>
2018-06-01 19:20:43 -0400

    vote-service init

---
+++ vote-service/src/main/java/com/pfe/ldb/vote/configuration/VoteServiceApplication.java
@@ -0,0 +1,12 @@
+package com.pfe.ldb.vote.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteServiceApplication.class, args);
+	}
+}
