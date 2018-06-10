8cf4cc08ffa19567609ed8365edf02704719ec15
walid bezzaoui <walid.bezzaoui.1@ens.etsmtl.ca>
2018-06-10 18:51:12 -0400

    calendar-service init config

---
+++ calendar-service/src/main/java/com/pfe/ldb/calendar/configuration/CalendarServiceApplication.java
@@ -0,0 +1,12 @@
+package com.pfe.ldb.calendar.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarServiceApplication.class, args);
+	}
+}
---
+++ calendar-service/src/test/java/com/pfe/ldb/calendar/configuration/CalendarServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pfe.ldb.calendar.configuration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
