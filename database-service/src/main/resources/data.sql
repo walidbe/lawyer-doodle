8c13c404d638ba6cb506ea644d6cff3ef6cf2990
walid bezzaoui <walid.bezzaoui.1@ens.etsmtl.ca>
2018-06-25 16:16:42 -0400

    Data SQL

--- database-service/src/main/resources/data.sql
+++ database-service/src/main/resources/data.sql
@@ -15,12 +15,31 @@ SET FOREIGN_KEY_CHECKS = 1;
 INSERT INTO TASK_GROUP(created_date, name, description,parent_id) VALUES(CURRENT_TIMESTAMP, 'Protocole', '',NULL);
 INSERT INTO TASK_GROUP(created_date, name, description,parent_id) VALUES(CURRENT_TIMESTAMP, 'Moyens préliminaires', '',(SELECT id FROM TASK_GROUP WHERE name = 'Protocole'));
 INSERT INTO TASK_GROUP(created_date, name, description,parent_id) VALUES(CURRENT_TIMESTAMP, 'Autres procédures', '',(SELECT id FROM TASK_GROUP WHERE name = 'Protocole'));
+INSERT INTO TASK_GROUP(created_date, name, description,parent_id) VALUES(CURRENT_TIMESTAMP, 'Défense', '',(SELECT id FROM TASK_GROUP WHERE name = 'Protocole'));
+INSERT INTO TASK_GROUP(created_date, name, description,parent_id) VALUES(CURRENT_TIMESTAMP, 'Expertises', '',(SELECT id FROM TASK_GROUP WHERE name = 'Protocole'));
+INSERT INTO TASK_GROUP(created_date, name, description,parent_id) VALUES(CURRENT_TIMESTAMP, 'INTERROGATOIRES', '',(SELECT id FROM TASK_GROUP WHERE name = 'Protocole'));
 
 INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Date de signification', 'Dernière date à laquelle la demande a été signifiée à toutes les parties', (SELECT id FROM TASK_GROUP WHERE name= 'Protocole'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Pièces – Communication aux demandeurs', '', (SELECT id FROM TASK_GROUP WHERE name= 'Moyens préliminaires'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Pièces – Communication aux demandeurs autres', '', (SELECT id FROM TASK_GROUP WHERE name= 'Moyens préliminaires'));
 INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Moyens déclinatoires', '', (SELECT id FROM TASK_GROUP WHERE name= 'Moyens préliminaires'));
 INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Moyens D’irrecevabilité', '', (SELECT id FROM TASK_GROUP WHERE name= 'Moyens préliminaires'));
 INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Autres Moyens Préliminaires', '', (SELECT id FROM TASK_GROUP WHERE name= 'Moyens préliminaires'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Sauvegarde', '', (SELECT id FROM TASK_GROUP WHERE name= 'Autres procédures'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Autres Incidents Procéduraux', '', (SELECT id FROM TASK_GROUP WHERE name= 'Autres procédures'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Pré-engagements', '', (SELECT id FROM TASK_GROUP WHERE name= 'INTERROGATOIRES'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Intérrogatoire par demandeur', '', (SELECT id FROM TASK_GROUP WHERE name= 'INTERROGATOIRES'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Intérrogatoire par défendeur', '', (SELECT id FROM TASK_GROUP WHERE name= 'INTERROGATOIRES'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Engagements de la demande', '', (SELECT id FROM TASK_GROUP WHERE name= 'INTERROGATOIRES'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Engagements de la défense', '', (SELECT id FROM TASK_GROUP WHERE name= 'INTERROGATOIRES'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Défense écrite', '', (SELECT id FROM TASK_GROUP WHERE name= 'Défense'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Défense reconventionnelle', '', (SELECT id FROM TASK_GROUP WHERE name= 'Défense'));
+INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Objections', '', (SELECT id FROM TASK_GROUP WHERE name= 'INTERROGATOIRES'));
 
+
+
+/*INSERT INTO TASK(created_date, name, description, task_group_id) VALUES(CURRENT_TIMESTAMP, 'Incidents', '', (SELECT id FROM TASK_GROUP WHERE name= 'Moyens préliminaires'));
+*/
 INSERT INTO MEMBER(created_date, email, first_name, last_name) VALUES(CURRENT_TIMESTAMP, 'walidrift@gmail.com', 'Walid', 'Bezzaoui');
 INSERT INTO USER(created_date, password, username, member_id)  VALUES(CURRENT_TIMESTAMP, '$2a$10$qwMriqG.D04PPneIIjC/l.xI/Gh2gPL1/wnJl2ILs/wNtXKNqHh66', 'walidrift@gmail.com', 1);
 
