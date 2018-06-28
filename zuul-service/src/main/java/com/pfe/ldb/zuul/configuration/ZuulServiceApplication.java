bec1f7fc0cb18f87b904c7afc40c742122fa7980
walid bezzaoui <walid.bezzaoui.1@ens.etsmtl.ca>
2018-06-28 09:49:29 -0400

    Zuul-Service

--- zuul-service/src/main/java/com/pfe/ldb/zuul/configuration/ZuulServiceApplication.java
+++ zuul-service/src/main/java/com/pfe/ldb/zuul/configuration/ZuulServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableEurekaClient
+@CrossOrigin(origins="http://localhost:3001")
 public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
