5d4754b07879593bceaf64f9e28f5b8bc5d9099d
walid bezzaoui <walid.bezzaoui.1@ens.etsmtl.ca>
2018-07-07 17:17:29 -0400

    Link user to Events Entity

---
+++ entity-management/src/main/java/com/pfe/ldb/entity/EventUserDestinationEntity.java
@@ -0,0 +1,57 @@
+package com.pfe.ldb.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="eventUserDestination")
+public class EventUserDestinationEntity extends AbstractEntity {
+
+	
+	public EventUserDestinationEntity() {
+		
+	}
+	public EventUserDestinationEntity(EventEntity event, String email, MemberEntity member) {
+		super();
+		this.event = event;
+		this.email = email;
+		this.member = member;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "eventId")
+	private EventEntity event;
+	
+	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "memberId")
+	private MemberEntity member;
+
+	public EventEntity getEvent() {
+		return event;
+	}
+
+	public void setEvent(EventEntity event) {
+		this.event = event;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public MemberEntity getMember() {
+		return member;
+	}
+
+	public void setMember(MemberEntity member) {
+		this.member = member;
+	}
+	
+}
